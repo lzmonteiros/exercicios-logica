25dee1274f91ebde2e2383249d59592e508136d0
Luiz Miguel <luizmms@outlook.com.br>
2025-05-28 17:07:28 -0300

    Adicionados os exercicios de javaScript 28/05/2025

---
+++ 01-script.js
@@ -0,0 +1,11 @@
+
+// SQ2 - ex 9
+// Menagem Secreta: Peça ao usuário para digitar uma senha e, senha for "abracadabra", exiba uma mensagem secreta
+
+let senha = prompt("Digite a senha: ")
+
+if(senha == "abracadabra"){
+alert("Senha está correta, seja muito bem vindo")
+}else{
+    alert("Senha incorreta")
+}
---
+++ 02-script.js
@@ -0,0 +1,11 @@
+// sq2 - ex 13
+// verificando votação: Peça ao usuário para inserir sua idade e verifique se ele é elegivel para votar (idade minima de 16 anos).
+
+let idade = prompt("Digite sua idade:")
+if(idade >= 16){
+    alert("Você é elegivel para votar.")
+
+}else{
+    alert("Você não é elegivel para votar.")
+
+}
